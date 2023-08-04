1303c93e908e5d4798c94ac722c56ac15da3058f
md. Alamin Faraji <alaminice1617@gmail.com>
2023-08-04 06:06:48 +0600

    about page done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,41 @@
-
-
+import './About.css'
+import { Player } from '@lottiefiles/react-lottie-player';
 const About = () => {
-    return (
-        <div className="bg-color h-screen text-white">
-            
-        </div>
-    );
+  return (
+    <div className="h-screen text-[#DDDDDD] mx-auto bg-color">
+      <h2 className="text-3xl font-bold mb-3 py-4 text-center">About Our Note Organization App</h2>
+     <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+     <div className='border mx-5 p-5 rounded-md shadow'>
+     <p className='text-xl text-justify'>
+        Welcome to our note organization app! This app is designed to help you keep your notes organized and easily accessible.
+        Whether you need to jot down quick ideas, create to-do lists, or save important information, our app has got you covered.
+      </p>
+      <p className='text-3xl py-3'>
+        Features of our app include:
+      </p>
+      <ul className="list-disc ml-6">
+        <li>Creating, editing, and deleting notes.</li>
+        <li>Assigning categories to your notes for easy filtering.</li>
+        <li>Attaching photos to your notes for better visualization.</li>
+        <li>Secure user authentication to protect your notes.</li>
+      </ul>
+      <p>
+        We hope you find our app useful in organizing your thoughts and tasks. If you have any feedback or suggestions, feel free to contact us.
+      </p>
+     </div>
+     <div className='bg-color'>
+     <Player
+         src='https://lottie.host/27f1cc4d-4190-44cd-a3d4-67e460bd9d12/tH1Y2Tq7kj.json'
+         className="player" 
+         loop 
+         autoplay
+         style={{ height: '400px', width: '400px' }}
+         speed={1}
+        />
+     </div>
+     </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -2,8 +2,29 @@
 
 const Feature = () => {
     return (
-        <div className="bg-color h-screen">
-            
+        <div className="bg-color h-screen text-[#DDDDDD]">
+        <h2 className="text-3xl font-bold mb-3 text-center">Features of Our App</h2>
+      <p>
+        Our app comes with a variety of features to make your note organization a breeze. Here are some key highlights:
+      </p>
+      <ul className="list-disc ml-6">
+        <li>
+          <strong>User Authentication:</strong> Securely sign up and log in to access your notes using Firebase authentication.
+        </li>
+        <li>
+          <strong>Create, Edit, and Delete Notes:</strong> Easily create new notes, edit existing ones, and delete notes when needed.
+        </li>
+        <li>
+          <strong>Note Categorization:</strong> Organize your notes into categories for better management and easy retrieval.
+        </li>
+        <li>
+          <strong>Cloud Storage:</strong> Store your notes securely in Firebase Cloud Firestore for easy access across devices.
+        </li>
+        <li>
+          <strong>Photo Attachments:</strong> Attach images or photos to your notes for enhanced content visualization.
+        </li>
+        {/* Add other features */}
+      </ul>
         </div>
     );
 };
