c39a2a86f1cd532f922a9c519c2e487cc4e7272d
md. Alamin Faraji <alaminice1617@gmail.com>
2023-08-04 06:29:50 +0600

    features page done

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -1,13 +1,15 @@
  import '../Home/Home.css';
-
+ import { Player } from '@lottiefiles/react-lottie-player';
 const Feature = () => {
     return (
         <div className="bg-color h-screen text-[#DDDDDD]">
-        <h2 className="text-3xl font-bold mb-3 text-center">Features of Our App</h2>
-      <p>
+        <h2 className="text-3xl font-bold py-5 text-center">Features of Our App</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-5'>
+            <div className='border mx-5 text-justify p-5'>
+            <p className='text-2xl'>
         Our app comes with a variety of features to make your note organization a breeze. Here are some key highlights:
       </p>
-      <ul className="list-disc ml-6">
+      <ul className="list-disc ml-6 text-lg">
         <li>
           <strong>User Authentication:</strong> Securely sign up and log in to access your notes using Firebase authentication.
         </li>
@@ -23,8 +25,21 @@ const Feature = () => {
         <li>
           <strong>Photo Attachments:</strong> Attach images or photos to your notes for enhanced content visualization.
         </li>
-        {/* Add other features */}
+       
       </ul>
+            </div>
+            <div className='bg-color'>
+
+            <Player
+         src='https://lottie.host/0bee4e94-e29e-4f0e-8605-8e5871fce399/2ACU5Ptob7.json'
+         className="player" 
+         loop 
+         autoplay
+         style={{ height: '400px', width: '400px' }}
+         speed={1}
+        />
+            </div>
+        </div>
         </div>
     );
 };
