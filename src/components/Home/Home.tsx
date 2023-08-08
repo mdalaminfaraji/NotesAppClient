e663a1f5fde104071400f1aaa0f6bc8ef5c770be
md. Alamin Faraji <alaminice1617@gmail.com>
2023-08-08 23:59:00 +0600

    update okey section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -22,10 +22,10 @@ const Home:React.FC = () => {
            <div className='bg-color h-screen pt-5 text-white'>
             <h1 style={{fontFamily:'Rencho'}}  className='text-center p-2 hidden md:flex md:text-4xl items-center justify-center '>The journey of a thousand notes begins with a single thought.</h1>
             <p style={{fontFamily:'Rencho'}} className='text-center p-3 text-3xl '>Organize your mind, organize your life.</p>
-            <div className='grid grid-cols-1 md:grid-cols-2 justify-center items-center'>
+            <div className='grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4'>
             <div className=' mx-auto'>
-                <p className='text-5xl '>Okay......</p>
-               <p className='text-3xl'>Let'st start with your first note!</p> 
+                <p className='text-3xl md:text-5xl '>Okay......</p>
+               <p className='text-2xl md:text-3xl '>Let'st start with your first note!</p> 
                <Link className='text-2xl underline text-blue-500 ' to={`${user?'/dashboard/addNote':'/signIn'}`}>Create one!</Link>
             </div>
            
